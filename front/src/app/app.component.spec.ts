d167d06da48efe2e4b234b4efac00be09bae61e9
Lara-cerq <71826548+Lara-cerq@users.noreply.github.com>
2024-07-15 15:32:14 +0200

    Update app.component.spec.ts

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -2,11 +2,13 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import {JokesService} from "./services/jokes.service";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
       imports: [HttpClientTestingModule],
       providers: [JokesService]
     }).compileComponents();
