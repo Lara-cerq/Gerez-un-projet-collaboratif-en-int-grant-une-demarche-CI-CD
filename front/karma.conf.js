19838cde608884a86dc4ca526273069f35611138
Lara Cerqueira <lara_cerqueira10@hotmail.com>
2024-08-02 15:15:30 +0200

    change github action and sonar-project.properties

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -28,7 +28,8 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, './coverage/bobapp'),
       subdir: '.',
       reporters: [
-        { type: 'lcov', subdir: 'lcov-report' },
+        { type: 'lcov' },
+        { type: 'html' },
         { type: 'text-summary' }
       ]
     },
